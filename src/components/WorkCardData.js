cd62cc04b1b3addaeff662ae14efa9f13b6ee17f
amit mishra <amitmishra677@gmail.com>
2023-09-19 22:43:35 +0530

    postcard changes

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,6 +1,7 @@
 import pro1 from "../assets/project1.png";
 import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.png";
+import pro4 from "../assets/project4.png";
 const ProjectCardData = [
   {
     imgsrc: pro1,
@@ -20,6 +21,13 @@ const ProjectCardData = [
     imgsrc: pro3,
     title: "Rymo Ecommerce Design",
     text: "Thriving Facebook community and top-ranked",
+
+    view: "https://www.youtube.com/watch?v=P8YuWEkTeuE&list=PL9bD98LkBR7P8MYh0RzNSHgeVNTA8g0nB&index=1&pp=iAQB",
+  },
+  {
+    imgsrc: pro4,
+    title: "Dimention Ecommerce Design",
+    text: "This is the latest design of dimention",
 
     view: "https://www.youtube.com/watch?v=P8YuWEkTeuE&list=PL9bD98LkBR7P8MYh0RzNSHgeVNTA8g0nB&index=1&pp=iAQB",
   },
